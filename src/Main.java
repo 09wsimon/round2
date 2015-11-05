ac1549c065c6220c8f44a2ee303b9ce67650b639
WestBrian <brianwest@client-75-102-66-180.mobility-up.psu.edu>
2015-11-04 19:23:37 -0500

    MVC example

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JFrame;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,12 +11,28 @@
  *
  * @author brianwest
  */
-public class Main {
+public class Main extends JFrame{
 
-    /**
-     * @param args the command line arguments
-     */
+    MainMenuView mainView;
+    MainMenuCntrl mainCtrl;
+    
+    public Main() {
+        super("Dino Dash!");
+        
+        setSize(800, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        mainView = new MainMenuView();
+        mainCtrl = new MainMenuCntrl(mainView);
+        
+        add(mainView);
+    }
+    
     public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setVisible(true);
     }
     
 }
--- src/MainMenuView.java
+++ src/MainMenuView.java
@@ -1,3 +1,8 @@
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +13,14 @@
  *
  * @author brianwest
  */
-public class MainMenuView {
+public class MainMenuView extends JPanel{
     
+    JButton button;
+    
+    public MainMenuView() {
+        setBackground(Color.WHITE);
+        
+        button = new JButton("Hello");
+        add(button);
+    }
 }
