d9f73c8baa2a07424f2753c11cb52d93e17c3a0f
natashabhave <bhave.natasha@gmail.com>
2015-11-18 19:20:21 -0500

    HighScore method

--- src/Dino.java
+++ src/Dino.java
@@ -1,7 +1,10 @@
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 
 /*
@@ -18,47 +21,64 @@ public class Dino extends Rectangle {
     private final int GRAVITY = 10;
     private final int dinoWidth = 75;
     private final int dinoHeight = 80;
-    
+
     private boolean isJumping;
     private boolean dirUp;
     private int speed;
     private ImageIcon dinoImage;
 
+    // Border variables
+    Polygon border;
+    int[] xpoints;
+    int[] ypoints;
+    int npoints;
+
     public Dino() {
         setBounds(10, 400, dinoWidth, dinoHeight);
+
+        border = createBorder();
 
         isJumping = false;
         dirUp = false;
         speed = 10;
-        
+
         dinoImage = new ImageIcon("src/images/dino_sm.png");
     }
 
     public void draw(Graphics g) {
+        border = createBorder();
+        
         if (!isJumping) {
             g.drawImage(dinoImage.getImage(), x, y, null);
-        }else{
+        } else {
             updateJump();
             g.drawImage(dinoImage.getImage(), x, y, null);
         }
+
+        if (DinoDash.showBounds) {
+            g.setColor(Color.RED);
+            //g.drawRect(x, y, width, height);
+            g.drawPolygon(border);
+        }
+
     }
 
     private void updateJump() {
         if (isJumping) {
             if (dirUp) {
                 y -= speed;
-                if (y < 300) {
+                if (y < 250) {
                     dirUp = false;
                 }
-            }else{
+            } else {
                 y += speed;
                 if (y >= 400) {
                     isJumping = false;
                 }
             }
-        }      
+        }
     }
-    
+
     public void jump() {
         isJumping = true;
         dirUp = true;
@@ -67,11 +87,16 @@ public class Dino extends Rectangle {
     public boolean isJumping() {
         return isJumping;
     }
-    
+
     public boolean checkHit(Rectangle r) {
-        if(intersects(r)) {
-            return true;
-        }
-        return false;
+        return border.intersects(r);
     }
+
+    private Polygon createBorder() {
+        xpoints = new int[]{x, x + width, x + width, x + (width / 2), x + (width / 2) / 2, x};
+        ypoints = new int[]{y, y, y + (height / 2), y + height, y + height, y + (height / 2) + (height / 4)};
+        npoints = xpoints.length;
+        return new Polygon(xpoints, ypoints, npoints);
+    }
+    
 }
--- src/GameBoardView.java
+++ src/GameBoardView.java
@@ -1,7 +1,10 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /*
@@ -9,19 +12,33 @@ import javax.swing.JPanel;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author brianwest
  */
-public class GameBoardView extends JPanel{
-    
+public class GameBoardView extends JPanel {
+
     public Dino dino;
     private ArrayList<Obstacle> obstacles;
+    private Image background;
+    private Image topBar;
     
+    public JButton showBoundsButton;
+    public int distance;
+    public int x = 0;
+    public int y = 100;
+
     public GameBoardView() {
         obstacles = new ArrayList<>();
         dino = new Dino();
+        distance = 0;
+
+        background = Toolkit.getDefaultToolkit().getImage("src/images/Scrollback.png");
+        topBar = Toolkit.getDefaultToolkit().getImage("src/images/topband.png");
+        
+        showBoundsButton = new JButton("Show Bounds");
+        showBoundsButton.setFocusable(false);
+        add(showBoundsButton);
     }
 
     @Override
@@ -30,35 +47,45 @@ public class GameBoardView extends JPanel{
 
         // Clear the screen
         g.clearRect(0, 0, getWidth(), getHeight());
-        
+
         // Draw the map
-        g.setColor(new Color(51, 204, 255));
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g.drawImage(topBar, 0, 0, null);
+        for (int i = 0; i < 10; i++) {
+            g.drawImage(background, x + (i * 100), y, null);
+        }
         g.setColor(Color.GREEN);
         g.fillRect(0, 400 + dino.height, getWidth(), getHeight() - (400 + dino.height));
-        
+
+        // Draw the distance
+        g.setColor(Color.RED);
+        g.drawString(Integer.toString(distance), getWidth() - 50, 20);
+
         // Draw the obstacles
-        for(Obstacle obstacle : obstacles) {
+        for (Obstacle obstacle : obstacles) {
             obstacle.draw(g);
         }
-        
+
         // Draw the dino
         dino.draw(g);
-        
+
         // Check collisions
-        for(Obstacle obstacle : obstacles) {
-            if(dino.checkHit(obstacle)) {
+        for (Obstacle obstacle : obstacles) {
+            if (dino.checkHit(obstacle)) {
                 g.setColor(Color.RED);
                 g.drawString("HIT!", 20, 20);
+                distance = 0;
             }
         }
     }
-    
+
+    // For some reason we need this to gain window focus
     @Override
     public void addNotify() {
         super.addNotify();
         requestFocus();
     }
+
+    // Getters and setters
     
     public ArrayList getObstacles() {
         return obstacles;
